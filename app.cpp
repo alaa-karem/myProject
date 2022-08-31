d144dcdd062fa0a72d063cb835957da02f3b5913
alaa <alaakarem@gmail.com>
2022-08-31 13:09:36 +0200

    creat cppFile

--- app.cpp
+++ app.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
+#define el "\n";
 #define Invincible ios_base::sync_with_stdio(0);  cin.tie(0);  cout.tie(0);
+typedef long long ll;
 int main()
 {
     Invincible
-    cout<<"Hello world";
+    cout<<"Hello GitHub"<<el
 }
